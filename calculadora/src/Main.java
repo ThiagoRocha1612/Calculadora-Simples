d33ccb690e2ea17fc2bbca1950ab7ccd66870e39
Thiago Rocha <Thiagomarlon1612@gmail.com>
2025-08-01 14:01:12 -0300

    Second Commit
    criar a classe Main com estrutura de método Main, e os import's necessários para o funcionamento.

---
+++ calculadora/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.inputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       //Adicionar a lógica após criar a classe calcular APP
+    }
+}
