f2915103df1247a92aec2a7e18f90f82b8710695
Thiago Rocha <Thiagomarlon1612@gmail.com>
2025-08-01 14:07:40 -0300

    Third Commit: Criar classe calculadorApp com seus métodos e import
    Criação da classe calculadorApp com método e seus import's necessários para a execução do programa.

---
+++ calculadora/src/calculadorApp.java
@@ -0,0 +1,8 @@
+import java.util.locate;
+import java.util.Scanner;
+
+public class calculadorApp {
+    public static void main(String[] args) {
+        //Adicionar logica e corrigir Import's
+    }
+}
