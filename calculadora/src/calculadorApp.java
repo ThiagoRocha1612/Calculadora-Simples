78d28fac78e3a197f4a53bbc4067d072a1bd5e2d
Thiago Rocha <Thiagomarlon1612@gmail.com>
2025-08-02 10:37:01 -0300

    Five Commit: Correção na criação das classes e métodos
    Correção na criação da classe calculadorApp e no método criado anteriormente, parte da lógica de calculo adicionada.

--- calculadora/src/calculadorApp.java
+++ calculadora/src/calculadorApp.java
@@ -2,7 +2,24 @@ import java.util.locate;
 import java.util.Scanner;
 
 public class calculadorApp {
-    public static void main(String[] args) {
-        //Adicionar logica e corrigir Import's
+    public static Double calculo(double valorUm, double valorDois, string op){
+        return switch (op) {
+            case "+" -> valorUm + valorDois;
+
+            case "-" -> valorUm - valorDois;
+
+            case "/" -> {
+                if (valorDois == 0) {
+                    System.out.println("Erro: Não é possível dividir por zero.");
+                    yield 0.0;
+                }
+                yield valorUm / valorDois;
+            }
+            case "*" -> valorUm * valorDois;
+            default -> {
+            System.out.println("Operação Inválida.");
+                yield 0.0;
+            }
+        };
     }
 }
